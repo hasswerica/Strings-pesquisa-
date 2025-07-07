d7dbaa4eb9ea453f2c92a7a6a9191899625421c8
hasswerica <141144963+hasswerica@users.noreply.github.com>
2025-07-07 11:33:33 +0000

    Aula 3

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave'); 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave); 
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-function mostraPalavrasChave() { 
-  alert('fui clicado!'); 
-} 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+  const palavras = texto.split(" ");
+
+  campoResultado.textContent = palavras.join(", ");
+}
